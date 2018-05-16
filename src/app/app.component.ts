35d16e16cfc612b10d87657ae0fd8459291a3b24
unknown <swapnilgajbhiye12@gmail.com>
2018-05-16 18:13:41 +0530

    Added Standings Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,24 @@ import { Component,ViewChild } from '@angular/core';
 import {Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import {StandingsPage} from '../pages/standings/standings'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
 
+  private homePage:any;
   @ViewChild(Nav) nav:Nav;
   rootPage:any = MyTeamsPage;
+  
+  
+
 
   pages: Array<{title:string,component:any}>
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
+    this.homePage = StandingsPage;
   }
 
   initializeApp(){
@@ -33,5 +38,15 @@ export class MyApp {
 
   goToTournaments(){
     this.nav.push(TournamentsPage); 
+  }
+
+  showStandings()
+  {
+
+   // this.nav.push(StandingsPage); 
+   this.rootPage=StandingsPage;
+
+    //this.pushPage=StandingsPage;
+    
   }
 }
--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the StandingsPage page.
@@ -7,7 +7,6 @@ import { NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
 @Component({
   selector: 'page-standings',
   templateUrl: 'standings.html',
