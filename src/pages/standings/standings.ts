6919aa7c21e20122bc994ac231f9d5e2d16744d8
unknown <swapnilgajbhiye12@gmail.com>
2018-05-16 23:35:58 +0530

    Added Points Table Grid

--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MyTeamsPage } from '../../pages/my-teams/my-teams';
 
 /**
  * Generated class for the StandingsPage page.
@@ -13,11 +14,17 @@ import {IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StandingsPage {
 
+  data=[{"id":"Mumbai Indians","P":2,"W":2,"L":1,"PtS":4},{"id":"Rajasthan Royals","P":2,"W":2,"L":1,"PtS":4},{"id":"Chennai Super Kings","P":2,"W":2,"L":1,"PtS":4},{"id":"Sunrisers Hyderabad","P":2,"W":2,"L":1,"PtS":4}]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StandingsPage');
+  }
+  goHome()
+  {
+    //this.navCtrl.push(MyTeamsPage);
+    this.navCtrl.setRoot(MyTeamsPage)
   }
 
 }
